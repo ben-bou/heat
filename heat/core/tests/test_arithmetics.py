d6b5ff0e05e56a8ee53741d60780ebe698933670
Michael Tarnawa <m.tarnawa@fz-juelich.de>
2019-09-16 09:02:05 +0200

    print values

--- heat/core/tests/test_arithmetics.py
+++ heat/core/tests/test_arithmetics.py
@@ -102,7 +102,7 @@ class TestArithmetics(unittest.TestCase):
         result_float = ht.array([1.5])
 
         self.assertTrue(ht.equal(ht.fmod(self.a_scalar, self.a_scalar), ht.float32([0.0])))
-        raise TypeError("0: {}, 1: {}, 2: {}, 3: {}".format(a_float.device.device_type, ht.array([[2.0],[2.0]]).device.device_type, ht.float32([0.0]).device.device_type, zero_tensor.device.device_type))
+        raise TypeError("0: {}, 1: {}, 2: {}, 3: {}".format(a_float.device, ht.array([[2.0],[2.0]]), ht.float32([0.0]), zero_tensor))
         self.assertTrue(ht.equal(ht.fmod(self.a_tensor, self.a_tensor), zero_tensor))
         self.assertTrue(ht.equal(ht.fmod(self.a_tensor, self.an_int_scalar), result))
         self.assertTrue(ht.equal(ht.fmod(self.a_tensor, self.another_tensor), result))
