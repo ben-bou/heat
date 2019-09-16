9b2a28fab52a8d84f00fc3fe1f85a7a01da7edac
Michael Tarnawa <m.tarnawa@fz-juelich.de>
2019-09-16 08:56:25 +0200

    set torch device to cuda globally

--- heat/core/tests/test_arithmetics.py
+++ heat/core/tests/test_arithmetics.py
@@ -9,6 +9,7 @@ import heat as ht
 if os.environ.get('DEVICE') == 'gpu':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     ht.use_device("gpu" if torch.cuda.is_available() else "cpu")
+    torch.cuda.device(device)
 
 class TestArithmetics(unittest.TestCase):
     @classmethod
