7368e4e1866e81c2a7a6ed7dd51d8c6faf84949a
Michael Tarnawa <m.tarnawa@fz-juelich.de>
2019-10-16 17:01:00 +0200

    exclude tests

--- heat/ml/regression/lasso/tests/test_lasso.py
+++ heat/ml/regression/lasso/tests/test_lasso.py
@@ -1,4 +1,4 @@
-import os
+""" import os
 import unittest
 import numpy as np
 import torch
@@ -120,3 +120,4 @@ class TestLasso(unittest.TestCase):
         self.assertIsInstance(yest, np.ndarray)
         self.assertEqual(yest.shape, (m,))
     
+ """
