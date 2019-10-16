237b563a69863b26d318821a782338080d8b7e49
Michael Tarnawa <m.tarnawa@fz-juelich.de>
2019-10-16 14:35:31 +0200

    fix old code

--- heat/core/dndarray.py
+++ heat/core/dndarray.py
@@ -1473,7 +1473,7 @@ class DNDarray:
             tensor_on_device : ht.DNDarray
                 A copy of this object on the GPU.
             """
-            self.__array = self.__array.cuda(devices.gpu_index())
+            self.__array = self.__array.cuda(devices.gpu)
             return self
 
     def __gt__(self, other):
